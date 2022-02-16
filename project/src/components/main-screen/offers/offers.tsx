a88c312a3b74daf9e2880705556ae8835ce655fa
kalaganov5 <kalaganov5@gmail.com>
2022-02-16 11:00:05 +0300

    Отдельные компоненты карточек

--- project/src/components/main-screen/offers/offers.tsx
+++ project/src/components/main-screen/offers/offers.tsx
@@ -1,40 +1,4 @@
-import {setRatingStyle} from 'utils/utils';
-
-function generateCard(): JSX.Element {
-  return (
-    <article className="cities__place-card place-card">
-      <div className="cities__image-wrapper place-card__image-wrapper">
-        <a href="/">
-          <img className="place-card__image" src="img/room.jpg" width="260" height="200" alt="" />
-        </a>
-      </div>
-      <div className="place-card__info">
-        <div className="place-card__price-wrapper">
-          <div className="place-card__price">
-            <b className="place-card__price-value">&euro;80</b>
-            <span className="place-card__price-text">&#47;&nbsp;night</span>
-          </div>
-          <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
-            <svg className="place-card__bookmark-icon" width="18" height="19">
-              <use xlinkHref="#icon-bookmark"></use>
-            </svg>
-            <span className="visually-hidden">In bookmarks</span>
-          </button>
-        </div>
-        <div className="place-card__rating rating">
-          <div className="place-card__stars rating__stars">
-            <span style={setRatingStyle(80)}></span>
-            <span className="visually-hidden">Rating</span>
-          </div>
-        </div>
-        <h2 className="place-card__name">
-          <a href="/">Wood and stone place</a>
-        </h2>
-        <p className="place-card__type">Private room</p>
-      </div>
-    </article>
-  );
-}
+import OfferItem from './offer-item/offer-item';
 
 function Offers(): JSX.Element {
   return (
@@ -59,11 +23,10 @@ function Offers(): JSX.Element {
             </ul>
           </form>
           <div className="cities__places-list places__list tabs__content">
-            {generateCard()}
-            {generateCard()}
-            {generateCard()}
-            {generateCard()}
-            {generateCard()}
+            {<OfferItem />}
+            {<OfferItem />}
+            {<OfferItem />}
+            {<OfferItem />}
           </div>
         </section>
         <div className="cities__right-section">
