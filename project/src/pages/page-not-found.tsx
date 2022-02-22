88f2c409bf5d5c617917c49fd0ffeaf49b507a64
kalaganov5 <kalaganov5@gmail.com>
2022-02-22 10:42:20 +0300

    Правка br

--- project/src/pages/page-not-found.tsx
+++ project/src/pages/page-not-found.tsx
@@ -2,7 +2,12 @@ import {Link} from 'react-router-dom';
 import {AppRoute} from 'const';
 
 function PageNotFound ():JSX.Element {
-  return (<p>404 Not Found :( <br/><Link to={AppRoute.Root}>Go to the homepage</Link></p>);
+  return (
+    <>
+      <p>404 Not Found :(</p>
+      <p><Link to={AppRoute.Root}>Go to the homepage</Link></p>
+    </>
+  );
 }
 
 export default PageNotFound;
