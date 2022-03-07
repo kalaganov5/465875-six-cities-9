9cb1b09724b73ed6d001777cbf179f7988185a23
kalaganov5 <kalaganov5@gmail.com>
2022-03-07 14:58:30 +0300

    mock отзывы

---
+++ project/src/mocks/reviews.ts
@@ -0,0 +1,28 @@
+import {Review} from '../types/app';
+
+export const reviews: Review[] = [
+  {
+    id: 1,
+    user: {
+      id: 1,
+      name: 'Angelina',
+      isPro: true,
+      avatar: 'avatar.svg',
+    },
+    comments: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.',
+    date: 'Mon Mar 07 2022 14:31:51 GMT+0300 (Moscow Standard Time)',
+    rating: 3.0,
+  },
+  {
+    id: 2,
+    user: {
+      id: 1,
+      name: 'Angelina',
+      isPro: true,
+      avatar: 'avatar.svg',
+    },
+    comments: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.',
+    date: 'Mon Mar 07 2022 14:31:51 GMT+0300 (Moscow Standard Time)',
+    rating: 3.0,
+  },
+];
--- project/src/types/app.ts
+++ project/src/types/app.ts
@@ -8,24 +8,28 @@ export type AppClassName = {
   [key: string]: PageCode;
 };
 
+type User = {
+  id: number,
+  name: string,
+  isPro: boolean,
+  avatar: string,
+}
+
+type Location = {
+  latitude: number,
+  longitude: number,
+  zoom: number,
+}
+
 export type Offer = {
   id: number,
   city: {
     name: string,
-    location: {
-      latitude: number,
-      longitude: number,
-      zoom: number,
-    },
+    location: Location,
   }
   title: string,
   description: string,
-  host: {
-    id: number,
-    name: string,
-    isPro: boolean,
-    avatar: string,
-  },
+  host: User,
   previewImage: string,
     images: {
       src: string,
@@ -34,14 +38,18 @@ export type Offer = {
   isFavorite: boolean,
   isPremium: boolean,
   rating: number,
-  location: {
-    latitude: number,
-    longitude: number,
-    zoom: number,
-  },
+  location: Location,
   type: string,
   bedrooms: number,
   maxAdults: number,
   price: number,
   options: string[],
+}
+
+export type Review = {
+  id: number,
+  user: User,
+  comments: string,
+  date: string,
+  rating: number,
 }
