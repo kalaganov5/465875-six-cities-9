b99304b8eec388784375a976157fa44553413880
kalaganov5 <kalaganov5@gmail.com>
2022-03-17 15:15:53 +0300

    Валидация формы

--- project/src/components/form-review/form-review.tsx
+++ project/src/components/form-review/form-review.tsx
@@ -2,10 +2,14 @@ import {useState, ChangeEvent} from 'react';
 import {ratingData} from 'const';
 
 function FormReview ():JSX.Element {
-  const [rating, setRating] = useState(0);
+  const [, setRating] = useState(0);
   const [review, setReview] = useState('');
-  // eslint-disable-next-line no-console
-  console.log(review, rating);
+  const [isRatingValid, setRatingValid] = useState(false);
+  const [isReviewValid, setReviewValid] = useState(false);
+
+  const reviewMinLength = 50;
+  const reviewMaxLength = 300;
+
   return (
     <form className="reviews__form form" action="#" method="post">
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
@@ -18,6 +22,11 @@ function FormReview ():JSX.Element {
                 id={`${item.value}-stars`} type="radio"
                 onChange={({ target }: ChangeEvent<HTMLInputElement>) => {
                   setRating(+target.value);
+                  if (+target.value >= 1) {
+                    setRatingValid(true);
+                  } else {
+                    setRatingValid(false);
+                  }
                 }}
               />
               <label htmlFor={`${item.value}-stars`} className="reviews__rating-label form__rating-label" title={item.name}>
@@ -35,6 +44,11 @@ function FormReview ():JSX.Element {
         onChange={
           ({target}: ChangeEvent<HTMLTextAreaElement>) => {
             setReview(target.value);
+            if (target.value.length >= reviewMinLength && target.value.length <= reviewMaxLength) {
+              setReviewValid(true);
+            } else {
+              setReviewValid(false);
+            }
           }
         }
         placeholder="Tell how was your stay, what you like and what can be improved"
@@ -44,7 +58,12 @@ function FormReview ():JSX.Element {
         <p className="reviews__help">
           To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
         </p>
-        <button className="reviews__submit form__submit button" type="submit" disabled>Submit</button>
+        <button className="reviews__submit form__submit button" type="submit"
+          disabled = {
+            !(isRatingValid && isReviewValid)
+          }
+        >Submit
+        </button>
       </div>
     </form>
   );
